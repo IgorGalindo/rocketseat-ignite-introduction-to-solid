2eebf4cb5f295eca75435ab02a7f36d1359afa75
Igor Galindo <igorgalindopaiva@gmail.com>
2022-07-04 18:00:18 -0300

    corrections finished

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -1,13 +1,24 @@
 import { Request, Response } from "express";
+import { User } from "modules/users/model/User";
 
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+    constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        const user_id = request.header("user_id");
+
+        let users: User[];
+
+        try {
+            users = this.listAllUsersUseCase.execute({ user_id });
+        } catch (err) {
+            return response.status(400).json({ error: err.message });
+        }
+
+        return response.json(users);
+    }
 }
 
 export { ListAllUsersController };
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -1,13 +1,24 @@
 import { Request, Response } from "express";
+import { User } from "modules/users/model/User";
 
 import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
 
 class TurnUserAdminController {
-  constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
+    constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        const { user_id } = request.params;
+
+        let user: User;
+
+        try {
+            user = this.turnUserAdminUseCase.execute({ user_id });
+        } catch (err) {
+            return response.status(404).json({ error: err.message });
+        }
+
+        return response.status(200).json(user);
+    }
 }
 
 export { TurnUserAdminController };
